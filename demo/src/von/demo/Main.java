e58b9938e91642384d47becce0fa8c8d2ed9922b
jeasonvon <jeasonvon@hotmail.com>
2015-01-17 13:33:51 +0800

    first git demo

---
+++ demo/src/von/demo/Main.java
@@ -0,0 +1,21 @@
+package von.demo;
+
+/**
+ * 这是一个简单点的demo，上传到git
+ * 
+ * @author Koku
+ * 
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		print();
+	}
+
+	public static void print() {
+		System.out.println("hello git !");
+	}
+}
